fe87765360488d1265a60f4fc77854eb60e46d96
Ale_Skora <ale.skora1@gmail.com>
2022-10-06 21:13:04 -0300

    Initial commit

---
+++ src/main/java/service/LivroService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.Livro;
+
+import java.util.List;
+
+public class LivroService {
+    public List<Livro> listarLivros() {
+        return null;
+    }
+
+    public Livro buscarLivro(String codigo) {
+        return null;
+    }
+
+    public Livro cadastrarLivro(String nome, String autor, String codigo) {
+        return null;
+    }
+}
---
+++ src/main/java/service/PessoaService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.Pessoa;
+
+import java.util.List;
+
+public class PessoaService {
+    public Pessoa cadastrarPessoa(String nome, String cpf, String endereco, String telefone) {
+        return null;
+    }
+
+    public List<Pessoa> listaPessoas() {
+        return null;
+    }
+
+    public Pessoa buscaPessoa(String cpf) {
+        return null;
+    }
+}
